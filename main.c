3c18158719011f3f90478878969dbcbf5743ddb2
sdmelen <dbezzhovchiy@gmail.com>
2023-10-20 00:55:14 +0300

    hs_0.0

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello brave new world!");
-    printf("something");
-    return 0;
-}
---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+
+enum buffs
+{
+    BF_TAUNT=0;
+    BF_WINDFURY=1;
+    BF_LIFESTEAL=2;
+};
+
+struct effects
+{
+    int buff;
+};
+
+struct card
+{
+    unsigned int cost;
+    unsigned int health;
+    unsigned int atack;
+    struct effects ef;
+};
+
+int main()
+{
+    printf("Hello brave new world!");
+    printf("something");
+    struct card c;
+    c.atack=2;
+    c.health=3;
+    c.cost=3;
+    c.ef.buff=BF_LIFESTEAL;
+    return 0;
+}
