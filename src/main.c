fa134db30305d4fbf9e69dab99cc9774e3ac83cd
Дмитрий Безжовчий <dbezzhovchiy@gmail.com>
2023-10-21 12:20:30 +0300

    mosckvorechye development

--- src/main.c
+++ src/main.c
@@ -6,16 +6,58 @@ int main()
     // ... ///
     
     // Создаем картонки
-    struct card c;
-    c.atack = 2;
-    c.health = 3;
-    c.cost = 3;
-    c.ef.buff = BF_LIFESTEAL;
+    struct card annoyer;
+    annoyer.atack = 1;
+    annoyer.health = 2;
+    annoyer.cost = 2;
+
+    int annoyer_characteristics[3];
+    annoyer_characteristics[0] = BF_TAUNT;
+    annoyer_characteristics[1] = BF_DIVINE_SHIELD;
+    annoyer_characteristics[2] = BF_NOTHING;
+    
+    annoyer.ef.buffs[0] = annoyer_characteristics[0];
+    annoyer.ef.buffs[1] = annoyer_characteristics[1];
+    annoyer.ef.buffs[2] = annoyer_characteristics[2];
 
     // Выводим карту юзера в консоль
-    // ... ///
+    output_card(annoyer);
 
     // Выводим какие то результаты
     printf("Nice HS Gameplay bro!");
+    getchar();
     return 0;
+}
+
+void output_card(struct card input_card)
+{
+    printf("health: %d", input_card.health);
+    printf("atack: %d", input_card.atack);
+    printf("cost: %d", input_card.cost);
+    i=1
+    while EARTH IS FLAT ...
+    {
+        i++
+        printf("%d", i)
+    }
+    for()
+}
+
+void inscription (int number)
+{
+    if (number == BF_NOTHING)
+        printf("Nothing");
+    else if(number == BF_TAUNT)
+        printf("Provocation");
+    else if(number == BF_WINDFURY)
+        printf("Windfury");
+    else if(number == BF_LIFESTEAL)
+        printf("Lifesteal");
+    else if (number == BF_DIVINE_SHIELD)
+        printf("Divine Shield");
+    else
+    {
+        printf("\a");
+        Beep(1000000, 1000000000000000);
+    }
 }
--- src/stdafx.h
+++ src/stdafx.h
@@ -1,16 +1,28 @@
-#include <stdio.h>
+#include <stdio.h>clear
+
 #include <windows.h>
+
+enum bufs_explained
+{
+    BFE_NOTHING = "Nothing",
+    BFE_TAUNT = "Provocation",
+    BFE_WINDFURY = "Windfury",
+    BFE_LIFESTEAL = "Lifesteal",
+    BFE_DIVINE_SHIELD = "Divine Shield"
+};
 
 enum buffs
 {
-    BF_TAUNT = 0,
-    BF_WINDFURY = 1,
-    BF_LIFESTEAL = 2
+    BF_NOTHING = 0,
+    BF_TAUNT = 1,
+    BF_WINDFURY = 2,
+    BF_LIFESTEAL = 3,
+    BF_DIVINE_SHIELD = 4
 };
 
 struct effects
 {
-    int buff;
+    int buffs[3];
 };
 
 struct card
@@ -20,3 +32,6 @@ struct card
     unsigned int atack;
     struct effects ef;
 };
+
+void output_card(struct card);
+void inscription (int number);
